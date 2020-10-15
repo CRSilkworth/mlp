e228154e5eda1b7da6bb17ff0018c3d4efb0525f
CRSilkworth <crsilkworth@gmail.com>
2020-10-14 21:59:51 -0400

    added copy_dir

--- mlp/utils/dirs.py
+++ mlp/utils/dirs.py
@@ -1,7 +1,7 @@
 import os
 import datetime
-from typing import Tuple, Text
-
+from typing import Tuple, Text, Optional, List
+import tensorflow as tf
 
 def pipeline_dirs(
   run_dir: Text,
@@ -37,3 +37,55 @@ def pipeline_dirs(
   pipeline_root = os.path.join(run_root, 'data')
 
   return proj_root, run_root, pipeline_root, serving_uri
+
+
+def lstrip_dirs(full_path, rm_path):
+  full_dirs = [d for d in full_path.split('/') if d != '']
+  rm_dirs = [d for d in rm_path.split('/') if d != '']
+
+  while rm_dirs:
+    rm_dir = rm_dirs.pop(0)
+    full_dir = full_dirs.pop(0)
+
+    if rm_dir != full_dir:
+      raise ValueError("{} not a prefix of {}.".format(rm_path, full_path))
+
+  if rm_dirs:
+    raise ValueError("{} not a prefix of {}.".format(rm_path, full_path))
+
+  return '/'.join(full_dirs)
+
+
+def copy_dir(
+  src_uri: Text,
+  dst_uri: Text,
+  ignore_subdirs: Optional[List[Text]] = None):
+
+  if ignore_subdirs is None:
+    ignore_subdirs = []
+  else:
+    ignore_subdirs = [os.path.join(src_uri, d) for d in ignore_subdirs]
+
+  tf.io.gfile.makedirs(dst_uri)
+
+  for src_dir_name, sub_dirs, file_names in tf.io.gfile.walk(src_uri):
+
+    skip_dir = False
+    for ignore_subdir in ignore_subdirs:
+
+      if src_dir_name.startswith(ignore_subdir):
+        skip_dir = True
+        break
+
+    if skip_dir:
+      continue
+
+    dst_dir_name = os.path.join(
+      dst_uri, lstrip_dirs(src_dir_name, src_uri)
+    )
+    tf.io.gfile.makedirs(dst_dir_name)
+
+    for file_name in file_names:
+      src_file_name = os.path.join(src_dir_name, file_name)
+      dst_file_name = os.path.join(dst_dir_name, file_name)
+      tf.io.gfile.copy(src_file_name, dst_file_name)
---
+++ mlp/utils/unit_test/dirs_test.py
@@ -0,0 +1,47 @@
+"""Tests for pandas_window_example_gen component."""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import tensorflow as tf
+import os
+import unittest
+import tempfile
+
+from mlp.utils.dirs import copy_dir
+
+
+class DirsTest(unittest.TestCase):
+
+  def copy_dir(self):
+    artifact_dirs = [
+      '2020-08-01-09-05-00/data/AlwaysPusher/pushed_model/40/',
+      '2020-08-01-09-05-00/data/AlwaysPusher/pushed_model/300/',
+      '2020-08-01-09-05-00/data/AlwaysPusher/pushed_model/55/'
+    ]
+    for i in range(len(artifact_dirs)):
+      with tempfile.TemporaryDirectory() as temp_dir:
+        base_dir = os.path.join(temp_dir, 'runs/tfx/pipeline')
+        for artifact_dir in artifact_dirs[:i + 1]:
+          artifact_path = os.path.join(base_dir, artifact_dir)
+          tf.io.gfile.makedirs(artifact_path)
+          self._touch(os.path.join(artifact_path, 'file_1.txt'))
+          self._touch(os.path.join(artifact_path, 'file_2.txt'))
+        src_pipeline_uri = os.path.join(base_dir, '2020-08-01-09-05-00')
+        dst_pipeline_uri = os.path.join(base_dir, '2020-08-01-09-05-01')
+        next_pipeline_uri = os.path.join(base_dir, '2020-08-01-09-05-02')
+
+        copy_dir(src_pipeline_uri, dst_pipeline_uri)
+        copy_dir(dst_pipeline_uri, next_pipeline_uri)
+
+
+  def _touch(self, path):
+    with open(path, 'a'):
+        os.utime(path, None)
+
+
+
+if __name__ == '__main__':
+  unittest.main()
+  # tf.test.main()
