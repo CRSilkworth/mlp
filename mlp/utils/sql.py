6729385d79137df6595b768831c51a43e04f1d61
CRSilkworth <crsilkworth@gmail.com>
2021-04-05 19:51:33 -0400

    added top_values sql query helper with test

--- mlp/utils/sql.py
+++ mlp/utils/sql.py
@@ -37,3 +37,39 @@ def rolling_query(query_file_name, partition_field, order_field, num_leading, fi
     **kwargs
   )
   return query
+
+
+def top_values(query_file_name, order_field, field_names, other_select=None, top_k=5, limit=None, **kwargs):
+
+  if other_select is None:
+    other_select = []
+
+  top_str = 'MAX(IF({order_field}={num}, {field_name}, NULL)) AS {new_field_name}_{adj_num}'
+  top_strs = []
+  for field_name in field_names:
+    new_field_name = field_name.split('.')[-1]
+    for num in range(top_k):
+      top_strs.append(top_str.format(
+        order_field=order_field,
+        field_name=field_name,
+        new_field_name=new_field_name,
+        num=num+1,
+        adj_num=num
+      ))
+
+  select_columns = '\n  , '.join(other_select + top_strs)
+
+  with open(query_file_name, 'r') as query_file:
+    query = query_file.read()
+
+  if limit is not None:
+    limit_str = "LIMIT {}".format(limit)
+  else:
+    limit_str = ''
+
+  query = query.format(
+    select_columns=select_columns,
+    limit=limit_str,
+    **kwargs
+  )
+  return query
--- mlp/utils/unit_test/sql_test.py
+++ mlp/utils/unit_test/sql_test.py
@@ -9,31 +9,40 @@ import os
 import unittest
 import tempfile
 
-from mlp.utils.sql import rolling_query
+from mlp.utils.sql import rolling_query, top_values
 
 
 class DirsTest(unittest.TestCase):
   def test_rolling_query(self):
-      with tempfile.TemporaryDirectory() as temp_dir:
-        query_file_name = os.path.join(temp_dir, 'query.sql')
-        with open(query_file_name, 'w') as query_file:
-          query_file.write('SELECT\n')
-          query_file.write('  {select_columns}\n')
-          query_file.write('FROM fake_table')
-        query = rolling_query(
-          query_file_name,
-          partition_field='part',
-          order_field='ord',
-          num_leading=4,
-          field_names=['field_a', 'field_b']
-        )
-        self.assertEqual(len(query), 669)
+    with tempfile.TemporaryDirectory() as temp_dir:
+      query_file_name = os.path.join(temp_dir, 'query.sql')
+      with open(query_file_name, 'w') as query_file:
+        query_file.write('SELECT\n')
+        query_file.write('  {select_columns}\n')
+        query_file.write('FROM fake_table')
+      query = rolling_query(
+        query_file_name,
+        partition_field='part',
+        order_field='ord',
+        num_leading=4,
+        field_names=['field_a', 'field_b']
+      )
+      self.assertEqual(len(query), 669)
 
-
-  def _touch(self, path):
-    with open(path, 'a'):
-        os.utime(path, None)
-
+  def test_top_values(self):
+    with tempfile.TemporaryDirectory() as temp_dir:
+      query_file_name = os.path.join(temp_dir, 'query.sql')
+      with open(query_file_name, 'w') as query_file:
+        query_file.write('SELECT {select_columns}\n')
+        query_file.write('FROM fake_table')
+      query = top_values(
+        query_file_name,
+        order_field='ord',
+        top_k=4,
+        other_select=['field_1', 'field_2'],
+        field_names=['field_a', 'field_b']
+      )
+      self.assertEqual(len(query), 418)
 
 
 if __name__ == '__main__':
