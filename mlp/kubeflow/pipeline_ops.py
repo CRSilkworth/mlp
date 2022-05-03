9172213d65606589962029bf6fea3565b37d5c27
CRSilkworth <crsilkworth@gmail.com>
2022-05-03 09:54:14 +0900

    added memory request and limit

--- mlp/kubeflow/pipeline_ops.py
+++ mlp/kubeflow/pipeline_ops.py
@@ -2,3 +2,14 @@ def set_gpu_limit(num_gpus, gpu_type='nvidia'):
   def _set_gpu_limit(container_op):
     container_op.set_gpu_limit(num_gpus, gpu_type)
   return _set_gpu_limit
+
+
+def set_memory_request_and_limits(memory_request, memory_limit):
+  def _set_memory_request_and_limits(task):
+
+    return (
+        task.container.set_memory_request(memory_request)
+            .set_memory_limit(memory_limit)
+    )
+
+  return _set_memory_request_and_limits
