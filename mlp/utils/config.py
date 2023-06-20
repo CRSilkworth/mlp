1147ec7d186c4cbc3a6c4c8688fbb841e6770418
CRSilkworth <crsilkworth@gmail.com>
2023-06-20 09:41:38 +0900

    switched sub dir order of pipeline_name and experiment

--- mlp/utils/config.py
+++ mlp/utils/config.py
@@ -8,108 +8,114 @@ from mlp.utils.sql import query_with_kwargs
 
 
 class VarConfig(object):
-  def __init__(self, file_path=None):
-    self.file_path = file_path
-    self.vars = {}
+    def __init__(self, file_path=None):
+        self.file_path = file_path
+        self.vars = {}
 
-    self.is_initialized = True
-    if file_path is not None and tf.io.gfile.exists(file_path):
-      self._load_from_json(file_path)
-    elif file_path is not None:
-      raise ValueError("{} not found".format(file_path))
+        self.is_initialized = True
+        if file_path is not None and tf.io.gfile.exists(file_path):
+            self._load_from_json(file_path)
+        elif file_path is not None:
+            raise ValueError("{} not found".format(file_path))
 
-  def _load_from_json(self, file_path):
-    with tempfile.TemporaryDirectory() as temp_dir:
-      temp_file_name = os.path.join(temp_dir, 'temp.py')
-      tf.io.gfile.copy(file_path, temp_file_name)
-      with open(temp_file_name, 'r') as temp_file:
-        vars = json.load(temp_file)
-    self.add_vars(**vars)
+    def _load_from_json(self, file_path):
+        with tempfile.TemporaryDirectory() as temp_dir:
+            temp_file_name = os.path.join(temp_dir, "temp.json")
+            tf.io.gfile.copy(file_path, temp_file_name)
+            with open(temp_file_name, "r") as temp_file:
+                vars = json.load(temp_file)
+        self.add_vars(**vars)
 
-  def _pipeline_params_from_json(self, file_path):
-    with open(file_path, 'r') as temp_file:
-      vars = json.load(temp_file)
-    self.add_vars(**vars)
-    
-    self.version = os.environ.get('PROJECT_VERSION')
-    self.add_vars(
-      **pipeline_var_names(
-        self.run_dir,
-        self.run_str,
-        self.mlp_project,
-        self.mlp_subproject,
-        self.runner,
-        self.pipeline_type,
-        self.experiment
-      )
-    )
-    schema_path = [self.run_root] + self.schema_params
-    self.schema_uri = os.path.join(*schema_path)
-    self.image_name = 'gcr.io/{gcp_project}/{mlp_project}:{version}'.format(
-      gcp_project=self.gcp_project,
-      mlp_project=self.mlp_project,
-      version=self.version
-    )
-    # If running with dataflow
-    self.beam_pipeline_args += [
-      '--project=' + self.gcp_project,
-      '--temp_location=' + os.path.join(self.run_root, 'tmp'),
-      '--region=' + self.gcp_region,
-      '--sdk_container_image=' + self.image_name,
-    ]
-    self.hash = self.get_hash()
-    self.write(self.vc_config_path)
-    if 'query_params' in self.get_vars():
-        self.query = query_with_kwargs(**self.query_params)
+    def _pipeline_params_from_json(self, file_path):
+        with open(file_path, "r") as temp_file:
+            vars = json.load(temp_file)
+        self.add_vars(**vars)
 
-  def __setattr__(self, name, value):
-    if hasattr(self, 'is_initialized') and self.is_initialized:
-      self.vars[name] = value
-      super(VarConfig, self).__setattr__(name, value)
-    else:
-      super(VarConfig, self).__setattr__(name, value)
+        self.version = os.environ.get("PROJECT_VERSION")
+        self.add_vars(
+            **pipeline_var_names(
+                self.run_dir,
+                self.run_str,
+                self.mlp_project,
+                self.mlp_subproject,
+                self.runner,
+                self.pipeline_type,
+                self.experiment,
+            )
+        )
+        schema_path = [self.run_root] + self.schema_params
+        self.schema_uri = os.path.join(*schema_path)
+        self.image_name = "gcr.io/{gcp_project}/{mlp_project}:{version}".format(
+            gcp_project=self.gcp_project,
+            mlp_project=self.mlp_project,
+            version=self.version,
+        )
+        # If running with dataflow
+        self.beam_pipeline_args += [
+            "--project=" + self.gcp_project,
+            "--temp_location=" + os.path.join(self.run_root, "tmp"),
+            "--region=" + self.gcp_region,
+            "--sdk_container_image=" + self.image_name,
+        ]
+        self.hash = self.get_hash()
+        self.write(self.vc_config_path)
+        if "query_params" in self.get_vars():
+            self.query = query_with_kwargs(**self.query_params)
 
-  def get_hash(self, max_len=8, var_to_ignore=None, remove_run_str=True):
-    if var_to_ignore is None:
-      var_to_ignore = ['hash']
+    def __setattr__(self, name, value):
+        if hasattr(self, "is_initialized") and self.is_initialized:
+            self.vars[name] = value
+            super(VarConfig, self).__setattr__(name, value)
+        else:
+            super(VarConfig, self).__setattr__(name, value)
 
-    if self.run_str is None:
-      run_str = ''
-    else:
-      run_str = self.run_str
+    def get_hash(self, max_len=8, var_to_ignore=None, remove_run_str=True):
+        if var_to_ignore is None:
+            var_to_ignore = ["hash"]
 
-    temp_d = {k: str(v).replace(run_str, '') for k, v in self.vars.items() if (k not in var_to_ignore and run_str not in k)}
+        if self.run_str is None:
+            run_str = ""
+        else:
+            run_str = self.run_str
 
-    hash = sha1(json.dumps(temp_d, sort_keys=True).encode('utf-8')).hexdigest()
-    if max_len is not None:
-      return hash[:max_len]
-    else:
-      return hash
+        temp_d = {
+            k: str(v).replace(run_str, "")
+            for k, v in self.vars.items()
+            if (k not in var_to_ignore and run_str not in k)
+        }
 
-  def get_vars(self):
-    r_dict = {}
-    r_dict.update(self.vars)
-    return r_dict
+        hash = sha1(json.dumps(temp_d, sort_keys=True).encode("utf-8")).hexdigest()
+        if max_len is not None:
+            return hash[:max_len]
+        else:
+            return hash
 
-  def write(self, file_path=None, overwrite=False):
-    if file_path is None:
-      raise ValueError("file_path is not set")
+    def get_vars(self):
+        r_dict = {}
+        r_dict.update(self.vars)
+        return r_dict
 
-    if tf.io.gfile.exists(file_path) and overwrite is True:
-      raise ValueError("{} exists and overwrite is set to False".format(file_path))
+    def write(self, file_path=None, overwrite=False):
+        if file_path is None:
+            raise ValueError("file_path is not set")
 
-    with tempfile.TemporaryDirectory() as temp_dir:
-      temp_file_name = os.path.join(temp_dir, 'temp.py')
+        if tf.io.gfile.exists(file_path) and overwrite is True:
+            raise ValueError(
+                "{} exists and overwrite is set to False".format(file_path)
+            )
 
-      with open(temp_file_name, 'w') as temp_file:
-        json.dump(self.vars, temp_file, indent=2, sort_keys=True)
+        with tempfile.TemporaryDirectory() as temp_dir:
+            temp_file_name = os.path.join(temp_dir, "temp.py")
 
-      save_dir = '/'.join(file_path.split('/')[:-1])
-      if not tf.io.gfile.exists(save_dir):
-        tf.io.gfile.makedirs(save_dir)
+            with open(temp_file_name, "w") as temp_file:
+                json.dump(self.vars, temp_file, indent=2, sort_keys=True)
 
-      tf.io.gfile.copy(temp_file_name, file_path)
+            save_dir = "/".join(file_path.split("/")[:-1])
+            if not tf.io.gfile.exists(save_dir):
+                tf.io.gfile.makedirs(save_dir)
 
-  def add_vars(self, **kwargs):
-    for key, val in kwargs.items():
-      setattr(self, key, val)
+            tf.io.gfile.copy(temp_file_name, file_path)
+
+    def add_vars(self, **kwargs):
+        for key, val in kwargs.items():
+            setattr(self, key, val)
--- mlp/utils/dirs.py
+++ mlp/utils/dirs.py
@@ -6,192 +6,174 @@ import tensorflow as tf
 
 
 def pipeline_dirs(
-  run_dir: Text,
-  run_str: Text,
-  mlp_project: Text,
-  mlp_subproject: Text,
-  pipeline_name: Text,
-  experiment: Optional[Text] = None) -> Tuple[Text, Text, Text, Text]:
-  """Get the standard directory names from the project names.
+    run_dir: Text,
+    run_str: Text,
+    mlp_project: Text,
+    mlp_subproject: Text,
+    pipeline_name: Text,
+    experiment: Optional[Text] = None,
+) -> Tuple[Text, Text, Text, Text]:
+    """Get the standard directory names from the project names.
 
-  Parameters
-  ----------
-  run_dir: Directory to store all pipeline runs.
-  run_str: The unique identifier of the
-  mlp_project: dskjl;
-  pipeline_name: dfi
+    Parameters
+    ----------
+    run_dir: Directory to store all pipeline runs.
+    run_str: The unique identifier of the
+    mlp_project: dskjl;
+    pipeline_name: dfi
 
-  Returns
-  -------
-  proj_root: The root directory of the mlp_project.
-  run_root: The root directory of one run of the mlp_subproject.
-  pipeline_root: The directory to write all the artifacts/metadata for the kubeflow pipeline.
-  serving_uri: The directory to write all final output objects (e.g. trained model.)
+    Returns
+    -------
+    proj_root: The root directory of the mlp_project.
+    run_root: The root directory of one run of the mlp_subproject.
+    pipeline_root: The directory to write all the artifacts/metadata for the kubeflow pipeline.
+    serving_uri: The directory to write all final output objects (e.g. trained model.)
 
-  """
-  if experiment is None:
-    pipeline_root = os.path.join(run_dir, 'tfx', pipeline_name)
-    serving_root = os.path.join(run_dir, 'serving')
-  else:
-    pipeline_root = os.path.join(run_dir, 'tfx', pipeline_name, experiment)
-    serving_root = os.path.join(run_dir, 'serving', experiment)
+    """
+    if experiment is None:
+        pipeline_root = os.path.join(run_dir, "tfx", pipeline_name)
+        serving_root = os.path.join(run_dir, "serving")
+    else:
+        pipeline_root = os.path.join(run_dir, "tfx", experiment, pipeline_name)
+        serving_root = os.path.join(run_dir, "serving", experiment)
 
-  if run_str is not None:
-    run_root = os.path.join(pipeline_root, run_str)
-  else:
-    run_str = datetime.datetime.utcnow().strftime('%Y-%m-%d-%H-%M-%S')
-    run_root = os.path.join(pipeline_root, run_str)
+    if run_str is not None:
+        run_root = os.path.join(pipeline_root, run_str)
+    else:
+        run_str = datetime.datetime.utcnow().strftime("%Y-%m-%d-%H-%M-%S")
+        run_root = os.path.join(pipeline_root, run_str)
 
-  data_root = os.path.join(run_root, 'data')
+    data_root = os.path.join(run_root, "data")
 
-  return pipeline_root, run_root, data_root, serving_root
+    return pipeline_root, run_root, data_root, serving_root
 
 
 def pipeline_var_names(
-  run_dir: Text,
-  run_str: Text,
-  mlp_project: Text,
-  mlp_subproject: Text,
-  runner: Text,
-  pipeline_type: Text,
-  experiment: Optional[Text] = None,
-  ):
+    run_dir: Text,
+    run_str: Text,
+    mlp_project: Text,
+    mlp_subproject: Text,
+    runner: Text,
+    pipeline_type: Text,
+    experiment: Optional[Text] = None,
+):
+    pipeline_name = "-".join([mlp_project, mlp_subproject, pipeline_type])
 
-  pipeline_name = '-'.join([
-    mlp_project,
-    mlp_subproject,
-    pipeline_type
-  ])
+    date = datetime.datetime.now().strftime("%Y_%m_%d")
+    kfp_pipeline_name = "-".join([pipeline_name, date])
 
-  date = datetime.datetime.now().strftime("%Y_%m_%d")
-  kfp_pipeline_name = '-'.join([pipeline_name, date])
+    pipeline_mod = ".".join([mlp_subproject, "pipelines", runner, pipeline_type])
 
-  pipeline_mod = '.'.join([
-    mlp_subproject,
-    'pipelines',
-    runner,
-    pipeline_type
-  ])
+    pipeline_root, run_root, data_root, serving_root = pipeline_dirs(
+        run_dir,
+        run_str,
+        mlp_project,
+        mlp_subproject,
+        pipeline_name,
+        experiment=experiment,
+    )
 
-  pipeline_root, run_root, data_root, serving_root = pipeline_dirs(
-    run_dir,
-    run_str,
-    mlp_project,
-    mlp_subproject,
-    pipeline_name,
-    experiment=experiment
-  )
-
-  return {
-    'pipeline_root': pipeline_root,
-    'run_root': run_root,
-    'data_root': data_root,
-    'serving_root': serving_root,
-    'serving_dir': os.path.join(serving_root, mlp_project, mlp_subproject),
-    'pipeline_mod': pipeline_mod,
-    'pipeline_name': pipeline_name,
-    'kfp_pipeline_name': kfp_pipeline_name,
-    'vc_config_path': os.path.join(run_root, 'config', 'pipeline_vars.json'),
-    'metadata_path': os.path.join(data_root, 'metadata', 'metadata.db')
-  }
+    return {
+        "pipeline_root": pipeline_root,
+        "run_root": run_root,
+        "data_root": data_root,
+        "serving_root": serving_root,
+        "serving_dir": os.path.join(serving_root, mlp_project, mlp_subproject),
+        "pipeline_mod": pipeline_mod,
+        "pipeline_name": pipeline_name,
+        "kfp_pipeline_name": kfp_pipeline_name,
+        "vc_config_path": os.path.join(run_root, "config", "pipeline_vars.json"),
+        "metadata_path": os.path.join(data_root, "metadata", "metadata.db"),
+    }
 
 
 def lstrip_dirs(full_path, rm_path):
-  full_dirs = [d for d in full_path.split('/') if d != '']
-  rm_dirs = [d for d in rm_path.split('/') if d != '']
+    full_dirs = [d for d in full_path.split("/") if d != ""]
+    rm_dirs = [d for d in rm_path.split("/") if d != ""]
 
-  while rm_dirs:
-    rm_dir = rm_dirs.pop(0)
-    full_dir = full_dirs.pop(0)
+    while rm_dirs:
+        rm_dir = rm_dirs.pop(0)
+        full_dir = full_dirs.pop(0)
 
-    if rm_dir != full_dir:
-      raise ValueError("{} not a prefix of {}.".format(rm_path, full_path))
+        if rm_dir != full_dir:
+            raise ValueError("{} not a prefix of {}.".format(rm_path, full_path))
 
-  if rm_dirs:
-    raise ValueError("{} not a prefix of {}.".format(rm_path, full_path))
+    if rm_dirs:
+        raise ValueError("{} not a prefix of {}.".format(rm_path, full_path))
 
-  return '/'.join(full_dirs)
+    return "/".join(full_dirs)
 
 
-def copy_dir(
-  src_uri: Text,
-  dst_uri: Text,
-  ignore_subdirs: Optional[List[Text]] = None):
-  """Copy directory to/from gcp and local."""
+def copy_dir(src_uri: Text, dst_uri: Text, ignore_subdirs: Optional[List[Text]] = None):
+    """Copy directory to/from gcp and local."""
 
-  if ignore_subdirs is None:
-    ignore_subdirs = []
-  else:
-    ignore_subdirs = [os.path.join(src_uri, d) for d in ignore_subdirs]
+    if ignore_subdirs is None:
+        ignore_subdirs = []
+    else:
+        ignore_subdirs = [os.path.join(src_uri, d) for d in ignore_subdirs]
 
-  tf.io.gfile.makedirs(dst_uri)
+    tf.io.gfile.makedirs(dst_uri)
 
-  for src_dir_name, sub_dirs, file_names in tf.io.gfile.walk(src_uri):
+    for src_dir_name, sub_dirs, file_names in tf.io.gfile.walk(src_uri):
+        skip_dir = False
+        for ignore_subdir in ignore_subdirs:
+            if src_dir_name.startswith(ignore_subdir):
+                skip_dir = True
+                break
 
-    skip_dir = False
-    for ignore_subdir in ignore_subdirs:
+        if skip_dir:
+            continue
 
-      if src_dir_name.startswith(ignore_subdir):
-        skip_dir = True
-        break
+        dst_dir_name = os.path.join(dst_uri, lstrip_dirs(src_dir_name, src_uri))
+        tf.io.gfile.makedirs(dst_dir_name)
 
-    if skip_dir:
-      continue
-
-    dst_dir_name = os.path.join(
-      dst_uri, lstrip_dirs(src_dir_name, src_uri)
-    )
-    tf.io.gfile.makedirs(dst_dir_name)
-
-    for file_name in file_names:
-      src_file_name = os.path.join(src_dir_name, file_name)
-      dst_file_name = os.path.join(dst_dir_name, file_name)
-      tf.io.gfile.copy(src_file_name, dst_file_name)
+        for file_name in file_names:
+            src_file_name = os.path.join(src_dir_name, file_name)
+            dst_file_name = os.path.join(dst_dir_name, file_name)
+            tf.io.gfile.copy(src_file_name, dst_file_name)
 
 
 def download_dir(
-  src_uri: Text,
-  dst_uri: Text,
-  ignore_subdirs: Optional[List[Text]] = None):
-  """Download directory from gcp bucket."""
-  if ignore_subdirs is None:
-    ignore_subdirs = []
-  else:
-    ignore_subdirs = [os.path.join(src_uri, d) for d in ignore_subdirs]
+    src_uri: Text, dst_uri: Text, ignore_subdirs: Optional[List[Text]] = None
+):
+    """Download directory from gcp bucket."""
+    if ignore_subdirs is None:
+        ignore_subdirs = []
+    else:
+        ignore_subdirs = [os.path.join(src_uri, d) for d in ignore_subdirs]
 
-  bucket_name = src_uri.replace('gs://', '').split('/')[0]
-  prefix = '/'.join(src_uri.replace('gs://', '').split('/')[1:])
+    bucket_name = src_uri.replace("gs://", "").split("/")[0]
+    prefix = "/".join(src_uri.replace("gs://", "").split("/")[1:])
 
-  if not bucket_name or not src_uri.startswith('gs://'):
-    raise ValueError("src_uri must start with gs://<bucket_name>. Got {}".format(src_uri))
+    if not bucket_name or not src_uri.startswith("gs://"):
+        raise ValueError(
+            "src_uri must start with gs://<bucket_name>. Got {}".format(src_uri)
+        )
 
-  storage_client = storage.Client()
-  bucket = storage_client.get_bucket(bucket_name)
-  blobs = bucket.list_blobs(prefix=prefix)
-  for blob in blobs:
-    src_file_name = blob.name.split('/')[-1]
-    if not src_file_name:
-      continue
+    storage_client = storage.Client()
+    bucket = storage_client.get_bucket(bucket_name)
+    blobs = bucket.list_blobs(prefix=prefix)
+    for blob in blobs:
+        src_file_name = blob.name.split("/")[-1]
+        if not src_file_name:
+            continue
 
-    src_dir_name = os.path.join(
-      'gs://', bucket_name, '/'.join(blob.name.split('/')[:-1])
-    )
-    skip_dir = False
+        src_dir_name = os.path.join(
+            "gs://", bucket_name, "/".join(blob.name.split("/")[:-1])
+        )
+        skip_dir = False
 
-    for ignore_subdir in ignore_subdirs:
+        for ignore_subdir in ignore_subdirs:
+            if src_dir_name.startswith(ignore_subdir):
+                skip_dir = True
+                break
 
-      if src_dir_name.startswith(ignore_subdir):
-        skip_dir = True
-        break
+        if skip_dir:
+            continue
 
-    if skip_dir:
-      continue
+        dst_dir_name = os.path.join(dst_uri, lstrip_dirs(src_dir_name, src_uri))
+        tf.io.gfile.makedirs(dst_dir_name)
 
-    dst_dir_name = os.path.join(
-      dst_uri, lstrip_dirs(src_dir_name, src_uri)
-    )
-    tf.io.gfile.makedirs(dst_dir_name)
-
-    print(dst_dir_name, src_file_name)
-    dst_file_name = os.path.join(dst_dir_name, src_file_name)
-    blob.download_to_filename(dst_file_name)
+        print(dst_dir_name, src_file_name)
+        dst_file_name = os.path.join(dst_dir_name, src_file_name)
+        blob.download_to_filename(dst_file_name)
